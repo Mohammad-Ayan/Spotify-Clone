49339933c26cfb0a0f45af62717595c4e38c22a4
Mohammad-Ayan <ayan2015786@gmail.com>
2024-09-04 16:51:32 +0530

    ,

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log("Let's Start");
+
+async function main() {
+
+    let a = await fetch("http://127.0.0.1:3000/Nasheed/")
+    let response = await a.text();
+    console.log(response)
+}
+
+main()
